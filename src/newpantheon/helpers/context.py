5fdca212380e9ec61b17f9c70e877b5e9628d313
Shinwoo Kim <shinwookim@proton.me>
2024-11-05 23:33:32 -0500

    fix: modernize code

--- src/newpantheon/helpers/context.py
+++ src/newpantheon/helpers/context.py
@@ -1,6 +1,6 @@
-import os
-from os import path
+from pathlib import Path
 import sys
-src_dir = path.abspath(path.join(path.dirname(__file__), os.pardir))
-base_dir = path.abspath(path.join(src_dir, os.pardir))
-sys.path.append(src_dir)
+
+src_dir = Path(__file__).resolve().parent.parent
+base_dir = src_dir.parent
+sys.path.append(str(src_dir))
